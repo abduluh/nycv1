1c5256156418ba093674eb239b6658682922e99b
NYC Search App <nyc-search@example.com>
2026-04-05 15:21:49 -0500

    Add Vercel config for monorepo deployment

---
+++ apps/web/.next/static/chunks/pages/_app-cb8cbeaddbc3f52f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{605:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(9903)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(605),n(7045))),_N_E=_.O()}]);
